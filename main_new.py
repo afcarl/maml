67be4e5c7d2c5f6af094f55c14577ac2cdaa8e66
csyanbin <csyanbin@gmail.com>
2017-08-07 02:33:09 +1000

    main new gating

--- main_new.py
+++ main_new.py
@@ -123,17 +123,18 @@ def train(model, saver, sess, exp_string, data_generator, resume_itr=0):
             input_tensors = [model.metatrain_op]
 
         if (itr % SUMMARY_INTERVAL == 0 or itr % PRINT_INTERVAL == 0):
-            input_tensors.extend([model.summ_op, model.total_loss1, model.total_losses2])
+            input_tensors.extend([model.summ_op, model.total_loss1, model.total_losses2], model.gating_distribution)
             if model.classification:
                 input_tensors.extend([model.total_accuracy1, model.total_accuracies2])
 
-        result = sess.run(input_tensors, feed_dict)
+        result = sess.run(input_tensors, feed_dict=feed_dict)
 
         if itr % SUMMARY_INTERVAL == 0:
             prelosses.append(result[-2])
             if FLAGS.log:
                 train_writer.add_summary(result[1], itr)
-            postlosses.append(result[-1])
+            postlosses.append(result[-1])a
+            gating = result[-3]
 
         if (itr!=0) and itr % PRINT_INTERVAL == 0:
             if itr < FLAGS.pretrain_iterations:
@@ -170,7 +171,7 @@ def train(model, saver, sess, exp_string, data_generator, resume_itr=0):
                 else:
                     input_tensors = [model.total_loss1, model.total_losses2]
             feed_dict[model.temp] = 0.0
-            result = sess.run(input_tensors, feed_dict)
+            result = sess.run(input_tensors, feed_dict=feed_dict)
             print('Validation results: ' + str(result[0]) + ', ' + str(result[1]))
 
     saver.save(sess, FLAGS.logdir + '/' + exp_string +  '/model' + str(itr))
@@ -305,7 +306,7 @@ def main():
         input_tensors = None
 
     model = MAML(dim_input, dim_output, test_num_updates=test_num_updates)
-    if FLAGS.train:
+    if FLAGS.train or not tf_data_load:
         model.construct_model(input_tensors=input_tensors, prefix='metatrain_')
     if tf_data_load:
         model.construct_model(input_tensors=metaval_input_tensors, prefix='metaval_')
